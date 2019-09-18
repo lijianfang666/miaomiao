cf1bddbe10b6088da292c50d669d35ae78b18770
lijianfang666 <lijianfang850@163.com>
2019-09-18 14:23:10 +0800

    add components

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="main">
+        <Header title="我的喵喵"></Header>
+        <login></login>
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import Header from '@/components/Header'
+    import TabBar from '@/components/TabBar'
+    import Login from '@/components/Login'
+    export default {
+        name: "Movie",
+        data() {
+            return {}
+        },
+        methods: {},
+        components : {
+            Header,TabBar,Login
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
