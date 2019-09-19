5b62c8ce0e8354cedff6ab925c6699a822cb325c
lijianfang666 <lijianfang850@163.com>
2019-09-19 10:45:41 +0800

    add setData

--- src/components/Clist/index.vue
+++ src/components/Clist/index.vue
@@ -13,88 +13,17 @@
         </div>
         <div class="cinema_body">
             <ul>
-                <li>
+                <li v-for="item in cinemasList" :key="item.id">
                     <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
+                        <span>{{item.nm}}</span>
+                        <span class="q"><span class="price">{{item.sellPrice}}</span> 元起</span>
                     </div>
                     <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
+                        <span>{{item.addr}}</span>
+                        <span>{{item.distance}}</span>
                     </div>
                     <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
-                    </div>
-                    <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
-                    </div>
-                    <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
-                    </div>
-                    <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
-                    </div>
-                    <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
-                    </div>
-                    <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
-                    </div>
-                    <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
-                    </div>
-                    <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
-                    </div>
-                    <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>大地影院(澳东世纪店)</span>
-                        <span class="q"><span class="price">22.9</span> 元起</span>
-                    </div>
-                    <div class="address">
-                        <span>金州区大连经济技术开发区澳东世纪3层</span>
-                        <span>1763.5km</span>
-                    </div>
-                    <div class="card">
-                        <div>小吃</div>
-                        <div>折扣卡</div>
+                        <div v-for="(num,key) in item.tag" v-if="num===1" :key="num.key" :class="key |classCard">{{key | formatCard}}</div>
                     </div>
                 </li>
             </ul>
@@ -106,9 +35,49 @@
     export default {
         name: "Clist",
         data() {
-            return {}
+            return {
+                cinemasList:[]
+            }
         },
-        methods: {}
+        methods: {},
+        mounted() {
+            this.axios.get('/api/cinemaList?cityId=10').then((res)=>{
+                var msg = res.data.msg;
+                if(msg === 'ok'){
+                   this.cinemasList = res.data.data.cinemas;
+                }
+            })
+        },
+        filters:{
+            formatCard(key){
+                var card = [
+                    {key:"allowRefund",val:"改签"},
+                    {key:"endorse",val:"退"},
+                    {key:"sell",val:"折扣卡"},
+                    {key:"snack",val:"小吃"}
+                ];
+                for(var i=0;i<card.length;i++){
+                    if(card[i].key===key){
+                        return card[i].val
+                    }
+                };
+                return "";
+            },
+            classCard(key){
+                var card = [
+                    {key:"allowRefund",val:"bl"},
+                    {key:"endorse",val:"bl"},
+                    {key:"sell",val:"or"},
+                    {key:"snack",val:"or"}
+                ];
+                for(var i=0;i<card.length;i++){
+                    if(card[i].key===key){
+                        return card[i].val
+                    }
+                };
+                return "";
+            }
+        }
     }
 </script>
 
@@ -126,5 +95,4 @@
     .cinema_body .card div{ padding: 0 3px; height: 15px; line-height: 15px; border-radius: 2px; color: #f90; border: 1px solid #f90; font-size: 13px; margin-right: 5px;}
     .cinema_body .card div.or{ color: #f90; border: 1px solid #f90;}
     .cinema_body .card div.bl{ color: #589daf; border: 1px solid #589daf;}
-
 </style>
